2c63a2cf450b85d04089f58f2bdbf21e0fb63bb8
Camilo-RBS <00083223@uca.edu.sv>
2024-10-26 11:45:36 -0600

    yeah :zap:

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv').config(); 
+
 const express = require('express');
 const path = require('path');
 const app = express();
-const routes = require('./Parcial02-Ejercicio01/routes/routes');
+const routes = require('./routes/routes');
 
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, '/Parcial02-Ejercicio01/views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', routes);
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log(`Servidor en marcha en el puerto ${PORT}`);
 });
